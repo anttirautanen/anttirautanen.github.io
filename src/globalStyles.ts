62c7708793610332e7c2a03cdd454f8aa0f5c5e0
Antti Rautanen <antti.v.j.rautanen@gmail.com>
2021-05-22 22:00:04 +0300

    Updates

--- src/globalStyles.ts
+++
@@ -1,8 +0,0 @@
-import { createGlobalStyle } from "styled-components"
-
-export const GlobalStyle = createGlobalStyle`
-  body {
-    background: linear-gradient(to bottom, #96BBBB 0%, #618985 100%);
-    height: 100vh;
-  }
-`
