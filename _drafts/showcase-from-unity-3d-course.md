3b411bd1ad917e4ae1c0437133f1f19c81098473
Antti Rautanen <anttirautanen@users.noreply.github.com>
2021-05-22 21:15:53 +0300

    Replace Jekyll with React app (#1)
    * Remove Jekyll
    
    * Install create react app
    
    * Add prettier and styled-components
    
    * Add tsconfig
    
    * Remove template content and add front page
    
    * Remove extra comments
    
    * Improve heading positioning

--- _drafts/showcase-from-unity-3d-course.md
+++
@@ -1,9 +0,0 @@
----
-layout: post
-author: Antti
-title: "Showcase: Argon Assault from Unity 3D Course on Udemy"
----
-
-Video of the game
-
-Learnings?
---
+++ src/globalStyles.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background: linear-gradient(to bottom, #96BBBB 0%, #618985 100%);
+    height: 100vh;
+  }
+`
