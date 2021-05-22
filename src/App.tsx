23ca55dbf0e47b7c280ca6e25b6a5596150fd307
Antti Rautanen <antti.v.j.rautanen@gmail.com>
2021-05-22 20:58:24 +0300

    Remove template content and add front page

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import styled from "styled-components"
+
+export const App = () => (
+  <Container>
+    <Heading>anttirautanen.com</Heading>
+  </Container>
+)
+
+const Container = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  height: 42vh;
+`
+
+const Heading = styled.div`
+  border-bottom: 1px solid #618985;
+  border-top: 1px solid #618985;
+  color: #618985;
+  font-family: "Bungee Hairline", cursive;
+  font-size: 64px;
+  padding: 20px 0;
+`
