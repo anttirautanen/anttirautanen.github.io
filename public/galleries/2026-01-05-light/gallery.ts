8cd1f772c79aad32016a9c8502ebe7ebba22e359
Antti Rautanen <anttirautanen@users.noreply.github.com>
2026-01-05 10:55:12 +0200

    Add a gallery

---
+++ public/galleries/2026-01-05-light/gallery.ts
@@ -0,0 +1,21 @@
+import { GalleryDescription } from "../../../src/gallery/types"
+
+const Gallery: GalleryDescription = {
+  name: "Valo",
+  date: "2026-01-05",
+  imageCount: 9,
+  captions: ["Tarkkailu", "Sarja", "Ruoste", "Puolet", "Suodatin", "Kimalle", "Läpäisy", "Silta", "Heijastus varjossa"],
+  grid: `
+    "c c c c"
+    "c c c c"
+    "b b e e"
+    "i i i i"
+    "i i i i"
+    "g g h h"
+    "a a d d"
+    "a a d d"
+    "f f f f"
+    `,
+}
+
+export default Gallery
